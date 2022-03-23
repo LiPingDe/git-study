96a4341c56274231f17e9d3aa8a297249042a7c5
liping <lipinguser@163.com>
2022-03-23 14:52:59 +0800

    third commit

--- src/main/java/com/lp/git/GitTest.java
+++ src/main/java/com/lp/git/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello 顾飞!");
-        System.out.println("hello 蒋丞!");
+        System.out.println("hello 蒋丞啊!");
     }
 }
