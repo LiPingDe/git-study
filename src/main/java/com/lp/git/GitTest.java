c5df348940fcb3e7775fc3f9ff9f105dc1403a58
liping <lipinguser@163.com>
2022-03-23 14:52:25 +0800

    second commit

--- src/main/java/com/lp/git/GitTest.java
+++ src/main/java/com/lp/git/GitTest.java
@@ -3,5 +3,7 @@ package com.lp.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello 顾飞!");
+        System.out.println("hello 蒋丞!");
     }
 }
