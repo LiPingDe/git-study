af58d139c0ba6698e13dd2e7327f62666690e138
liping <lipinguser@163.com>
2022-03-23 14:59:59 +0800

    hot-fix commit1

--- src/main/java/com/lp/git/GitTest.java
+++ src/main/java/com/lp/git/GitTest.java
@@ -1,9 +1,12 @@
 package com.lp.git;
 
+import javax.sound.midi.Soundbank;
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello 顾飞!");
         System.out.println("hello 蒋丞啊!");
+        System.out.println("hello 李平");
     }
 }
